af841e4d07c0be8118b6e4630ade9a61cc495ae4
user <user@ubuntu.(none)>
2011-04-19 14:50:34 -0400

    Lots of changes to finger.cpp. added threads added libfreenect device instead of clnui drivers

--- finger.cpp
+++ finger.cpp
@@ -1,7 +1,38 @@
+/*
+Kinect Mouse
+NYU - Motion Capture Class Spring 2011 Project
+http://movement.nyu.edu/mocap11s/
+This file was adapted from OpenKinect Project's glview.c demo file
+That file was modified by Tim Flaman to X11 mouse movement which inspired this file
+This version of the file adds more advanced openCV hand tracking
+ * This file is part of the OpenKinect Project. http://www.openkinect.org
+ *
+ * Copyright (c) 2010 individual OpenKinect contributors. See the CONTRIB file
+ * for details.
+ *
+ * This code is licensed to you under the terms of the Apache License, version
+ * 2.0, or, at your option, the terms of the GNU General Public License,
+ * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
+ * or the following URLs:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * If you redistribute this file in source form, modified or unmodified, you
+ * may:
+ *   1) Leave this header intact and distribute it under the same terms,
+ *      accompanying it with the APACHE20 and GPL20 files, or
+ *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
+ *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
+ * In all cases you must keep the copyright notice intact and include a copy
+ * of the CONTRIB file.
+ *
+ * Binary distributions must follow the binary distribution requirements of
+ * either License.
+ */
 #include <iostream>
 
 // tuio
-#include "TuioServer.h"
+//#include "TuioServer.h"
 
 // opencv
 #pragma comment(lib, "cxcore210d.lib")
@@ -10,16 +41,27 @@
 #include <opencv/highgui.h>
 #include <opencv/cv.h>
 
+#include <pthread.h>
+
 // kinect
-#include <CLNUIDevice.h>
-#pragma comment(lib, "CLNUIDevice.lib")
+//#include <CLNUIDevice.h>
+//#pragma comment(lib, "CLNUIDevice.lib")
 
 //MOUSE
-//X11 control
+//X11 control Should be able to delete these after utouch integration
 #include <assert.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <X11/extensions/XTest.h>
+
+#include <evemu.h>//utouch multi-touch event emulation
+
+#include <libfreenect.h>//kinect driver by openkinect
+freenect_context *f_ctx;
+freenect_device *f_dev;
+int freenect_angle = 17;
+int freenect_led;
+
 int snstvty = 0;
 float pointerx = 0, pointery = 0;
 float mousex = 0, mousey = 0;
@@ -27,6 +69,70 @@ float tmousex = 0, tmousey = 0;
 int screenw = 0, screenh = 0;
 int pusx = 0, pusy = 0;
 
+uint8_t gl_depth_front[640*480*4];
+uint8_t gl_depth_back[640*480*4];
+
+uint8_t gl_rgb_front[640*480*4];
+uint8_t gl_rgb_back[640*480*4];
+
+pthread_t freenect_thread;
+pthread_mutex_t gl_backbuf_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp)
+{
+	int i;
+	int first = 0;
+	int px = 0 , py = 0;
+	int tx = 0 , ty = 0; //counts what row and what column we are in
+	int alert = 0;
+	uint16_t *depth = v_depth;
+	
+	pthread_mutex_lock(&gl_backbuf_mutex);
+	for (i=0; i<FREENECT_FRAME_PIX; i++) {
+		int pval = t_gamma[depth[i]];
+		int lb = pval & 0xff;
+		
+		tx++;
+		if(tx >= 640) {
+			tx = 0;		
+			ty++;
+		}
+		/*case 0-5*/
+		switch (pval>>8) {
+			case 0:
+				gl_depth_back[3*i+0] = 255;
+				gl_depth_back[3*i+1] = 0;
+				gl_depth_back[3*i+2] = 0;
+				alert++;
+				if (!first) { 
+					first = i;
+					px = tx;
+					py = ty;				
+				}
+				break;
+			case 1:
+				gl_depth_back[3*i+0] = 255;
+				gl_depth_back[3*i+1] = 255;
+				gl_depth_back[3*i+2] = 255;
+				break;
+			default:
+				gl_depth_back[3*i+0] = 0;
+				gl_depth_back[3*i+1] = 0;
+				gl_depth_back[3*i+2] = 0;
+				break;
+		}
+	}
+	if(alert > snstvty) {	
+		printf("\n!!!TOO CLOSE!!!\n");
+	}
+	//Ready to pass gl_deapth_back to openCV processing. 1st convert matrix
+	
+	
+	got_depth++;
+	pthread_cond_signal(&gl_frame_cond);
+	pthread_mutex_unlock(&gl_backbuf_mutex);
+}
 void unproject(unsigned short* depth, float* x, float* y, float* z) {
 	int u,v;
 	const float f = 500.0f;
@@ -186,20 +292,64 @@ int mouse(Scalar& center){
 
 }
 
+void rgb_cb(freenect_device *dev, void *rgb, uint32_t timestamp)
+{
+	pthread_mutex_lock(&gl_backbuf_mutex);
+	got_frames++;
+	memcpy(gl_rgb_back, rgb, FREENECT_VIDEO_RGB_SIZE);
+	pthread_cond_signal(&gl_frame_cond);
+	pthread_mutex_unlock(&gl_backbuf_mutex);
+}
+
+void printUsage() {
+	printf("\n=====Kinect Mouse=====\n\n"
+	"Synopsis:\n"
+	"\tkmouse [sensitivity (1-32767)]\n"
+	"'W'-Tilt Up\n'S'-Level\n'X'-Tilt Down\n'0'-'6'-LED Mode\n");
+}
+
+void *freenect_threadfunc(void *arg) {
+	freenect_set_tilt_degs(f_dev,freenect_angle);
+	freenect_set_led(f_dev,LED_GREEN);
+	freenect_set_depth_callback(f_dev, depth_cb);
+	freenect_set_video_callback(f_dev, rgb_cb);
+	freenect_set_video_format(f_dev, FREENECT_VIDEO_RGB);
+	freenect_set_depth_format(f_dev, FREENECT_DEPTH_11BIT);
+	
+	while ( (key = cvWaitKey(1)) != 27) { //while escape key is not pressed	
+		freenect_raw_tilt_state* state;
+		freenect_update_tilt_state(f_dev);
+		state = freenect_get_tilt_state(f_dev);;
+		double dx,dy,dz;
+		freenect_get_mks_accel(state, &dx, &dy, &dz);
+		//printf("\r raw acceleration: %4d %4d %4d  mks acceleration: %4f %4f %4f\r", ax, ay, az, dx, dy, dz);
+		fflush(stdout);
+	}
+	printf("\nShutting Down Streams...\n");
+
+	freenect_stop_depth(f_dev);
+	freenect_stop_video(f_dev);
+
+	freenect_close_device(f_dev);
+	freenect_shutdown(f_ctx);
+
+	printf("-- done!\n");
+	}
+
 void main(int argc, char **argv) {
 	using namespace std;
 	using namespace cv;
-	using namespace TUIO;
+//	using namespace TUIO;
 
 	//Initializing Mouse Stuff
-
+	printUsage();
 	if (argc == 2) {
 		snstvty = atoi(argv[1]);
 	} else {
 		snstvty = 20000;
 	}
 
-	mousemask(ALL_MOUSE_EVENTS, NULL);
+	mousemask(ALL_MOUSE_EVENTS, NULL); //What does this do? Where is it?
 
 	display = XOpenDisplay(0);
 
@@ -223,7 +373,7 @@ void main(int argc, char **argv) {
 
 	Mat1f debugFrame(480, 640);
 
-	CLNUICamera cam = CreateNUICamera();
+	//CLNUICamera cam = CreateNUICamera();
 
 	const float rMin = 25;
 
@@ -232,19 +382,41 @@ void main(int argc, char **argv) {
 	unsigned short* depthFrameRawData = (unsigned short*) depthFrameRaw.data;
 	int key;
 
-	bool tuio3dMode = false;
-	TuioServer* tuio = new TuioServer(tuio3dMode);
-	TuioTime time;
+//	bool tuio3dMode = false;
+//	TuioServer* tuio = new TuioServer(tuio3dMode);
+//	TuioTime time;
 
 	bool debug = true;
 
 	float zMin = 0.0f;
 	float zMax = 0.75f;
 
-	if ( StartNUICamera(cam) ) {
-		cout << "cam started" << endl;
-		while ( (key = cvWaitKey(1)) != 27) {
-			if ( GetNUICameraDepthFrameRAW(cam, (PUSHORT)depthFrameRaw.data) ) {
+	if (!freenect_init(&f_ctx, NULL)) {
+		printf("freenect_init() failed\n");
+		return 1;
+	}
+	freenect_set_log_level(f_ctx, FREENECT_LOG_DEBUG);//should look into how to read this log
+	
+	int nr_devices = freenect_num_devices (f_ctx);
+	printf ("\nNumber of Devices Found: %d\n", nr_devices);
+
+	if (nr_devices < 1) {
+		printf("\nCOULD NOT LOCATE KINECT :(\n");
+		return 1;
+		}
+
+	if (freenect_open_device(f_ctx, &f_dev, user_device_number) < 0) {
+		printf("\nCOULD NOT LOCATE KINECT :(\n");
+		return 1;
+		}
+		
+	res = pthread_create(&freenect_thread, NULL, freenect_threadfunc, NULL);
+	if (res) {
+		printf("Could Not Create Thread\n");
+		return 1;
+	}
+		
+	if ( GetNUICameraDepthFrameRAW(cam, (PUSHORT)depthFrameRaw.data) ) {
 				unproject(depthFrameRawData, (float*)x.data, (float*)y.data, (float*)z.data);
 				debugFrame = z * 0.1; 
 
@@ -267,7 +439,7 @@ void main(int argc, char **argv) {
 					}					
 				}
 
-				// send fingertip positions via TUIO
+				/* send fingertip positions via TUIO
 				time = TuioTime::getSessionTime();
 				tuio->initFrame(time);
 				for(vector<Point2i>::iterator it = fingerTips.begin(); it != fingerTips.end(); it++) {
@@ -292,13 +464,10 @@ void main(int argc, char **argv) {
 
 				cout << tuio->getTuioCursors().size() << endl;
 				int mouse();
-				// draw our debugframe
-				imshow("debugFrame", debugFrame);
+				*/// draw our debugframe
+				//imshow("debugFrame", debugFrame); //required?
 			}
-		}
-	} else {
-		cout << "could not start cam" << endl;
-	}
+		
 
-	StopNUICamera(cam);
+	//StopNUICamera(cam);
 }
